00fd298a27d4b2654ad20de1059e0e95d427350f
Jonaki Medda <jmedda@uvic.ca>
2021-12-05 14:28:19 -0800

    Added manual input box

--- ECE569/homepage/views.py
+++ ECE569/homepage/views.py
@@ -15,8 +15,8 @@ class FindCity(object):
       context = {}
       my_ip = requests.get('https://api.ipify.org').text
       # print(my_ip)
-      geolocation_api = "your api"
-      # my_ip = "192.168.1.67"
+      geolocation_api = "0e89983e41344f97a25ae1804602f0ac"
+      my_ip = "192.168.1.67"
       request_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + geolocation_api + '&ip_address=' + my_ip
       response = requests.get(request_url)
       result = response.content.decode()
@@ -42,7 +42,6 @@ def upload(request):
          print(city)
          context={'city':city}
          if city is not 'True':
-
             path = os.path.expanduser("~")
             local_dir = os.path.join(path,"Documents")
             folder_check = os.path.isdir(local_dir)
@@ -52,7 +51,7 @@ def upload(request):
             name = fs.save(uploaded_file.name, uploaded_file)        
             messages.success(request, 'File saved successfully in Documents')
          else:
-            messages.warning(request, 'City name not found and subit again')
+            messages.warning(request, 'City name not found and submit again')
       # context['url'] = fs.url(name)
 
       if value == "cloud":
@@ -67,7 +66,33 @@ def upload(request):
       # return render(request, 'upload.html')
 
 
+def manual_upload(request):
+   context = {}
+   if request.method == 'POST':
+      uploaded_file = request.FILES['upload']
+      value = request.POST.get("msg")
+      if value == "local":
+         city = request.POST.get("inp")
+         print(city)
+         context = {'city': city}
+         if city is 'True':
+            path = os.path.expanduser("~")
+            local_dir = os.path.join(path, "Documents")
+            folder_check = os.path.isdir(local_dir)
+            path = os.path.join(local_dir, city)
+            fs = FileSystemStorage(location=path)
+            # fs = FileSystemStorage(location=local_dir)
+            name = fs.save(uploaded_file.name, uploaded_file)
+            messages.success(request, 'File saved successfully in Documents')
+      if value == "cloud":
+               upload = UploadFile.objects.create(file=uploaded_file)
+               messages.success(request, 'File saved successfully in S3 bucket')
 
+               # upload = UploadFile.objects.all()
+               # fs = FileSystemStorage()
+               # name = fs.save(uploaded_file.name, uploaded_file)
+               # context['url'] = fs.url(name)
+   return render(request, 'upload.html', context)
 
 
 
