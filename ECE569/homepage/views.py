487ade936ddfdeee92988649284faa7e2f50a8d1
karthiga <karthigathangavel.2011@gmail.com>
2021-12-05 23:44:11 -0800

    final

--- ECE569/homepage/views.py
+++ ECE569/homepage/views.py
@@ -13,10 +13,10 @@ class FindCity(object):
 
    def get_ip_location(self):
       context = {}
-      # my_ip = requests.get('https://api.ipify.org').text
+      my_ip = requests.get('https://api.ipify.org').text
       # print(my_ip)
       geolocation_api = "0e89983e41344f97a25ae1804602f0ac"
-      my_ip = "192.168.1.67"
+      # my_ip = "192.168.1.67"
       request_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + geolocation_api + '&ip_address=' + my_ip
       response = requests.get(request_url)
       print(type(response))
