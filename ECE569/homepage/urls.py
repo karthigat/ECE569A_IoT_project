e98238f691e599f0b066d795820e3ced3a116c39
karthiga <karthigathangavel.2011@gmail.com>
2021-12-05 23:40:39 -0800

    final commit

--- ECE569/homepage/urls.py
+++ ECE569/homepage/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import include, url
 urlpatterns = [
    #path('', views.page, name='Mainpage'),
    path('',views.upload,name="index"),
+   # path('',views.upload_city,name="index"),
 
   ]
 
--- ECE569/homepage/views.py
+++ ECE569/homepage/views.py
@@ -13,12 +13,13 @@ class FindCity(object):
 
    def get_ip_location(self):
       context = {}
-      my_ip = requests.get('https://api.ipify.org').text
+      # my_ip = requests.get('https://api.ipify.org').text
       # print(my_ip)
-      geolocation_api = "your api"
-      # my_ip = "192.168.1.67"
+      geolocation_api = "0e89983e41344f97a25ae1804602f0ac"
+      my_ip = "192.168.1.67"
       request_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + geolocation_api + '&ip_address=' + my_ip
       response = requests.get(request_url)
+      print(type(response))
       result = response.content.decode()
       result = json.loads(result)
       city = result['city']
@@ -33,16 +34,11 @@ class FindCity(object):
 def upload(request):
    context={}
    if request.method == 'POST':
-      print('1')
       uploaded_file = request.FILES['upload']
-      value = request.POST.get("msg")
-      if value == "local":
-         city_obj = FindCity()
-         city = city_obj.get_ip_location()
-         print(city)
-         context={'city':city}
-         if city is not 'True':
-
+      # manual location
+      if request.POST.get("get") == 'submit to local' or request.POST.get("get") == 'submit to cloud':
+         city = request.POST.get("city")
+         if request.POST.get("get") == 'submit to local':
             path = os.path.expanduser("~")
             local_dir = os.path.join(path,"Documents")
             folder_check = os.path.isdir(local_dir)
@@ -51,23 +47,39 @@ def upload(request):
             # fs = FileSystemStorage(location=local_dir)
             name = fs.save(uploaded_file.name, uploaded_file)        
             messages.success(request, 'File saved successfully in Documents')
-         else:
-            messages.warning(request, 'City name not found and subit again')
-      # context['url'] = fs.url(name)
+         if request.POST.get("get") == 'submit to cloud':
+            upload = UploadFile.objects.create(file=uploaded_file)        
+            messages.success(request, 'File saved successfully in S3 bucket')
+      else:    
+         # geolocation
+         uploaded_file = request.FILES['upload']
+         value = request.POST.get("msg")
+         if value == "local":
+            city_obj = FindCity()
+            city = city_obj.get_ip_location()
+            print(city)
+            context={'city':city}
+            if city is not 'True':
 
-      if value == "cloud":
-         upload = UploadFile.objects.create(file=uploaded_file)        
-         messages.success(request, 'File saved successfully in S3 bucket')
-         
+               path = os.path.expanduser("~")
+               local_dir = os.path.join(path,"Documents")
+               folder_check = os.path.isdir(local_dir)
+               path = os.path.join(local_dir, city)
+               fs = FileSystemStorage(location=path)
+               # fs = FileSystemStorage(location=local_dir)
+               name = fs.save(uploaded_file.name, uploaded_file)        
+               messages.success(request, 'File saved successfully in Documents')
+            else:
+               messages.warning(request, 'City name not found and subit again')
+         # context['url'] = fs.url(name)
+
+         if value == "cloud":
+            upload = UploadFile.objects.create(file=uploaded_file)        
+            messages.success(request, 'File saved successfully in S3 bucket')
+            
       # upload = UploadFile.objects.all()
       # fs = FileSystemStorage()
       # name = fs.save(uploaded_file.name, uploaded_file)
       # context['url'] = fs.url(name)
    return render(request, 'upload.html', context)
       # return render(request, 'upload.html')
-
-
-
-
-
-
