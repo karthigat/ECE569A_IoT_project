6278a809d0a62238d93178f39dd05487b401bbd9
Jonaki Medda <jmedda@uvic.ca>
2021-12-05 23:36:54 -0800

    Added requirements file

--- ECE569/homepage/views.py
+++ ECE569/homepage/views.py
@@ -16,7 +16,7 @@ class FindCity(object):
       my_ip = requests.get('https://api.ipify.org').text
       # print(my_ip)
       geolocation_api = "0e89983e41344f97a25ae1804602f0ac"
-      my_ip = "192.168.1.67"
+      #my_ip = "192.168.1.67"
       request_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + geolocation_api + '&ip_address=' + my_ip
       response = requests.get(request_url)
       result = response.content.decode()
